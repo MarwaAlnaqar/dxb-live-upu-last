63e319ce9032947e7caa53744af59ae06c76a37b
MarwaAlnaqar <marwa.alnaqar@dwtc.com>
2025-08-19 12:47:53 -0700

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,16 @@ import './App.css'
 
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Questions from './pages/questions';
-import QuestionAnalytics from'./pages/QuestionAnalytics';
+// import Questions from './pages/questions';
+// import QuestionAnalytics from'./pages/QuestionAnalytics';
 import QuestionsPrivate from'./pages/privatePages/QuestionsPrivate';
-import QuestionPrivateAnalytics from'./pages/privatePages/QuestionPrivateAnalytics';
+// import QuestionPrivateAnalytics from'./pages/privatePages/QuestionPrivateAnalytics';
 import QuestionPrivateAnalyticsV2 from'./pages/privatePages/QuestionPrivateAnalyticsV2';
-import QuestionAnalyticsUpdated from'./pages/QuestionAnalyticsUpdated';
+// import QuestionAnalyticsUpdated from'./pages/QuestionAnalyticsUpdated';
 import QuestionAnalyticsUpdated2 from'./pages/QuestionAnalyticsUpdated2';
 
 
-import CountryAnalytics from './pages/CountryAnalytics';
+// import CountryAnalytics from './pages/CountryAnalytics';
 import CountryAnalyticsV2 from './pages/CountryAnalyticsv2';
 
 
@@ -29,9 +29,10 @@ function App() {
   return (
     <Router >
       <Routes>
-        <Route path="/" element={<Questions />}  />
+        {/* <Route path="/" element={<Questions />}  /> */}
         <Route path="/country-analytics" element={<CountryAnalyticsV2 />} />
         <Route path="/question-analytics" element={<QuestionAnalyticsUpdated2 />} />
+        <Route path="/question-private" element={<QuestionsPrivate />} />
         {/* <Route path="/country-analytics" element={<CountryAnalytics />} />
         <Route path="/country-analytics-v2" element={<CountryAnalyticsV2 />} />
 
